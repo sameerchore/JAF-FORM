a6ad3a48044ec1632f3a2cb3da80fad653ae19f0
Sameer <sameerchore5@gmail.com>
2026-01-28 02:05:55 +0530

    deploy dist for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/JAF-FORM/'   // ✅ MATCH REPO NAME EXACTLY
+  base: '/JAF-FORM/' 
+  
 })
+
+
